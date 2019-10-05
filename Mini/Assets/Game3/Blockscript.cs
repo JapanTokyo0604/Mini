fe73c5fa0d5219c6f4e1e90c42bcf16204c008bb
JapanTokyo0604 <53828195+JapanTokyo0604@users.noreply.github.com>
2019-10-05 16:57:48 +0900

    initial commit
    intial commit
    ・Play1_Game1 ~ 3
    Temporary completion
    ・Play1_Game4
    incomplete...

---
+++ Mini/Assets/Game3/Blockscript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blockscript : MonoBehaviour
+{
+
+    public GameObject block;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+
+            Destroy(block);
+            Play1_Game3.addScore();
+            Play1_Game3.addDesBlock();
+    }
+
+}
